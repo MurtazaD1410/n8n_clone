eb94ea2c24a636ff39cfc51d5310106bc4d456f1
MurtazaD1410 <dhariwalamurtaza22@gmail.com>
2025-10-22 00:03:30 +0200

    08: Sentry Error Tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,9 @@
-import prisma from "@/lib/db";
 import { inngest } from "@/src/inngest/client";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
 import { createOpenAI } from "@ai-sdk/openai";
 import { createAnthropic } from "@ai-sdk/anthropic";
+import * as Sentry from "@sentry/nextjs";
 
 const google = createGoogleGenerativeAI();
 const openai = createOpenAI();
@@ -13,6 +13,13 @@ export const execute = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
   async ({ event, step }) => {
+    console.warn("Something is missing");
+    console.error("Something is wrong");
+
+    Sentry.logger.info("User triggered test log", {
+      log_source: "sentry_test",
+    });
+
     const { steps: geminiSteps } = await step.ai.wrap(
       "gemini-generate-text",
       generateText,
@@ -20,33 +27,48 @@ export const execute = inngest.createFunction(
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant",
         prompt: "What is 2+2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
-    const { steps: openAiSteps } = await step.ai.wrap(
-      "openai-generate-text",
-      generateText,
-      {
-        model: openai("gpt-4"),
-        system: "You are a helpful assistant",
-        prompt: "What is 2+2?",
-      }
-    );
+    // const { steps: openAiSteps } = await step.ai.wrap(
+    //   "openai-generate-text",
+    //   generateText,
+    //   {
+    //     model: openai("gpt-4"),
+    //     system: "You are a helpful assistant",
+    //     prompt: "What is 2+2?",
+    //     experimental_telemetry: {
+    //       isEnabled: true,
+    //       recordInputs: true,
+    //       recordOutputs: true,
+    //     },
+    //   }
+    // );
 
-    const { steps: antropicSteps } = await step.ai.wrap(
-      "antropic-generate-text",
-      generateText,
-      {
-        model: anthropic("claude-sonnet-4-5"),
-        system: "You are a helpful assistant",
-        prompt: "What is 2+2?",
-      }
-    );
+    // const { steps: antropicSteps } = await step.ai.wrap(
+    //   "antropic-generate-text",
+    //   generateText,
+    //   {
+    //     model: anthropic("claude-sonnet-4-5"),
+    //     system: "You are a helpful assistant",
+    //     prompt: "What is 2+2?",
+    //     experimental_telemetry: {
+    //       isEnabled: true,
+    //       recordInputs: true,
+    //       recordOutputs: true,
+    //     },
+    //   }
+    // );
 
     return {
       geminiSteps,
-      openAiSteps,
-      antropicSteps,
+      // openAiSteps,
+      // antropicSteps,
     };
   }
 );
